a586e36ff6d73c8d864a3991a6c5b1c027a4c515
Mikhail Kozlov <33799845+Borntowarn@users.noreply.github.com>
2023-11-29 09:52:40 +0300

    Update translate.py

--- nn/utils/translate.py
+++ nn/utils/translate.py
@@ -1,4 +1,3 @@
-# IAM_TOKEN = 't1.9euelZqczY7PlsvMic-elYuXxo-Qxu3rnpWalsrHipfPx82TipWWl4nKlpTl9PdLc3de-e9vbETa3fT3CyJ1Xvnvb2xE2g.3qy2cwY74DIU4OgYWS9AFg7av1cRNjh5kM_3Yc5oD4nsrE-UDNODEQu--eK6oHqpxYeXt-wLGHLTE01H0EWeCA'
 # folder_id = 'b1giso3qle0a0jtnj2bh'
 # target_language = 'ru'
 
